abd5af4c1cf25d4da26fdef0cfe42837fe033dc4
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2023-10-20 16:48:15 +1300

    fix scip auto-install

--- inst/build_scip.sh
+++ inst/build_scip.sh
@@ -15,6 +15,7 @@ if test -z "${CMAKE_EXE}"; then
 fi
 
 : ${R_HOME=`R RHOME`}
+RSCRIPT_BIN=${R_HOME}/bin/Rscript
 if test -z "${R_HOME}"; then
     echo "'R_HOME' could not be found!"
     exit 1
@@ -22,17 +23,19 @@ fi
 
 R_SCIP_PKG_HOME=`pwd`
 SCIP_SRC_FILE=`find "$(pwd -P)" -name "scipopt*"`
-SCIP_SRC_DIR=${R_SCIP_PKG_HOME}/inst/`basename ${SCIP_SRC_FILE} .tgz`
-R_SCIP_BUILD_DIR=${SCIP_SRC_DIR}/build
-R_SCIP_LIB_DIR=${R_SCIP_PKG_HOME}/src/SCIPlib
+SCIP_SRC_DIR=`basename ${SCIP_SRC_FILE} .tgz`
+R_SCIP_SRC_DIR=`${RSCRIPT_BIN} -e "cat(tools::R_user_dir('rscip'))"`
+R_SCIP_LIB_DIR=${R_SCIP_SRC_DIR}/sciplib
+R_SCIP_BUILD_DIR=${R_SCIP_SRC_DIR}/sciplib/build
 
 echo ""
 echo "[FILES AND FOLDERS]"
 echo "R_SCIP_PKG_HOME = '${R_SCIP_PKG_HOME}'"
-echo "SCIP_SRC_DIR = '${SCIP_SRC_DIR}'"
 echo "SCIP_SRC_FILE = '${SCIP_SRC_FILE}'"
-echo "R_SCIP_BUILD_DIR = '${R_SCIP_BUILD_DIR}'"
+echo "SCIP_SRC_DIR = '${SCIP_SRC_DIR}'"
+echo "R_SCIP_SRC_DIR = '${R_SCIP_SRC_DIR}'"
 echo "R_SCIP_LIB_DIR = '${R_SCIP_LIB_DIR}'"
+echo "R_SCIP_BUILD_DIR = '${R_SCIP_BUILD_DIR}'"
 
 export CC=`"${R_HOME}/bin/R" CMD config CC`
 export CXX=`"${R_HOME}/bin/R" CMD config CXX`
@@ -54,32 +57,20 @@ echo "CXX11: '${CXX11}'"
 # extract scipoptsuite
 echo ""
 echo "[EXTRACTION]"
-cd ${R_SCIP_PKG_HOME}/inst
-tar -xzvf ${SCIP_SRC_FILE}
-mkdir -p ${R_SCIP_BUILD_DIR}
+tar -xzf ${SCIP_SRC_FILE} -C ${R_SCIP_SRC_DIR}
+rm -f ${SCIP_SRC_FILE}
+mv ${R_SCIP_SRC_DIR}/${SCIP_SRC_DIR} ${R_SCIP_LIB_DIR}
 
 # config makefile
 echo ""
 echo "[CONFIGURATION]"
+mkdir -p ${R_SCIP_BUILD_DIR}
 cd ${R_SCIP_BUILD_DIR}
-CMAKE_OPTS="-DIPOPT=off -DGMP=on -DZIMPL=off -DREADLINE=off"
-# CMAKE_OPTS="-DIPOPT=off -DGMP=on -DZIMPL=off -DREADLINE=off -DTPI=tny"
-#CMAKE_OPTS="-DAUTOBUILD=on"
+CMAKE_OPTS="-DIPOPT=off -DGMP=on -DZIMPL=off -DREADLINE=off -DTPI=tny -DCMAKE_POSITION_INDEPENDENT_CODE:bool=ON -DSHARED:bool=on"
 ${CMAKE_EXE} .. ${CMAKE_OPTS} -G "Unix Makefiles"
 
 # build scip
 echo ""
 echo "[BUILDING]"
-MAKE_OPTS="ZLIB=false READLINE=false SHARED=true"
+MAKE_OPTS="ZLIB=false READLINE=false TPI=tny SHARED=true"
 ${MAKE} libscip ${MAKE_OPTS}
-
-# copy to package directory
-echo ""
-echo "[INSTALLATION]"
-mkdir -p ${R_SCIP_LIB_DIR}
-mkdir -p ${R_SCIP_LIB_DIR}/lib
-mkdir -p ${R_SCIP_LIB_DIR}/include
-mkdir -p ${R_SCIP_LIB_DIR}/config
-cp -R ${R_SCIP_BUILD_DIR}/lib/. ${R_SCIP_LIB_DIR}/lib
-cp -R ${SCIP_SRC_DIR}/scip/src/. ${R_SCIP_LIB_DIR}/include
-cp -R ${R_SCIP_BUILD_DIR}/scip/. ${R_SCIP_LIB_DIR}/config
